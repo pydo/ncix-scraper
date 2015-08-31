01601490d1d38a09923e160e217133c294612ef7
pydo <toqisgarde513@hushmail.com>
2015-08-31 14:43:02 -0700

    removed sets, now the db get populated properly

--- ncix_scraper.py
+++ ncix_scraper.py
@@ -3,6 +3,7 @@ from lxml.html.clean import Cleaner
 import requests
 import sqlite3
 from time import sleep
+from datetime import datetime
 
 
 def get_categories():
@@ -14,21 +15,22 @@ def get_categories():
         scrape_ncix(category)
 
 def scrape_ncix(category):
-    sleep(5)
+    sleep(2)
     page = requests.get(category)
     tree = html.fromstring(page.text)
     titles = tree.xpath('//span[@class="listing"]/a/text()')
     prices = tree.xpath('//td[@class="line"]/font/strong/text()')
-    add_to_db(zip(set(titles), set(prices)))
+    add_to_db(zip(titles, prices, [datetime.now()]*len(titles)))
 
 def add_to_db(products):
     conn = sqlite3.connect('products.db')
     with conn:
         c = conn.cursor()
-        table = 'CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, titles TEXT, prices FLOAT)'
+        table = 'CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, titles TEXT, prices FLOAT, date_added TIMESTAMP)'
         c.execute(table)
-        c.executemany('''INSERT INTO products (titles, prices) VALUES (?,?)''', products)
+        c.executemany('''INSERT INTO products (titles, prices, date_added) VALUES (?,?,?)''', products)
 
 
 if __name__ == '__main__':
     get_categories()
+    # print(datetime.now())
